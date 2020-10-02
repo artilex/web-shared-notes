15bbdd38b617d1842ea72a847ee2eab411cf8c00
Alexey Rodionov <leksurion@gmail.com>
2020-10-02 12:26:07 +0300

    Initial commit

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { RootContainer } from './HomePageStyles';
+
+const HomePage = () => {
+    return (
+      <RootContainer>
+        Hey man
+      </RootContainer>
+    );
+}
+
+export default HomePage;
